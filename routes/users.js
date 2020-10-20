4cac416d267abfedfc71df4cb0e6a8acdbdd7bc1
Bala Sridhar <balasridhar1986@gmail.com>
2020-10-20 15:30:55 -0500

    backend code.

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,65 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
+const User = require('../models/User');
 // @route   POST    api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-  res.send("Register's a user");
-});
+router.post(
+  '/',
+  [
+    check('name', 'name is required').not().isEmpty(),
+    check('email', 'please include a valid email').isEmail(),
+    check(
+      'password',
+      'please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User Already exists' });
+      }
+
+      user = new User({ name, email, password });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+      //res.send('User Saved: ' + name);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Server Error: ' + err.message);
+    }
+  }
+);
 
 module.exports = router;
